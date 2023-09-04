9e92a56a640c1cb538203a1b82ef998bc374f8ac
Enzo Michel Barbosa Feldman <enzomichelfeldman@gmail.com>
2023-09-04 10:56:50 -0300

    My solution to this exercise

--- 08_calculator/solution/calculator-solution.js
+++ 08_calculator/solution/calculator-solution.js
@@ -1,46 +1,41 @@
-const add = function (a, b) {
-  return a + b;
+const add = function(a, b) {
+  return a + b
 };
 
-const subtract = function (a, b) {
-  return a - b;
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function (array) {
-  return array.reduce((total, current) => total + current, 0);
+const sum = function(array) {
+	return array.reduce((total, current) => total + current, 0);
 };
 
-const multiply = function (array) {
-  return array.reduce((product, current) => product * current)
+const multiply = function(array) {
+  return array.reduce((total, current) => total * current);
 };
 
-const power = function (a, b) {
-  return Math.pow(a, b);
+const power = function(a, b) {
+	return a ** b
 };
 
-const factorial = function (n) {
-  if (n === 0) return 1;
-  let product = 1;
-  for (let i = n; i > 0; i--) {
-    product *= i;
-  }
-  return product;
-};
+const factorial = function(a) {
+	if (a < 0) 
+        return -1;
+    
+  else if (a == 0) 
+      return 1;
+    
+    else {
+        return (a * factorial(a - 1));
+  };
+}
 
-// This is another implementation of Factorial that uses recursion
-// THANKS to @ThirtyThreeB!
-const recursiveFactorial = function (n) {
-  if (n === 0) {
-    return 1;
-  }
-  return n * recursiveFactorial(n - 1);
-};
-
+// Do not edit below this line
 module.exports = {
   add,
   subtract,
   sum,
   multiply,
   power,
-  factorial,
+  factorial
 };
