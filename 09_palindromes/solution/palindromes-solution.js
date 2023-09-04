a0331d1f272c7d2958fd3a837201fc285edd83d0
Enzo Michel Barbosa Feldman <enzomichelfeldman@gmail.com>
2023-09-04 11:07:00 -0300

    My solution

--- 09_palindromes/solution/palindromes-solution.js
+++ 09_palindromes/solution/palindromes-solution.js
@@ -1,6 +1,11 @@
-const palindromes = function (string) {
-  const processedString = string.toLowerCase().replace(/[^a-z0-9]/g, "");
-  return processedString.split("").reverse().join("") == processedString;
+const palindromes = function (str) {
+    const cleanStr = str.replace(/[^A-Za-z0-9]/g, '').toLowerCase();
+    let reversed = "";
+    reversed = cleanStr.split('').reduce((reversed, char) => char + reversed, '');
+
+    if(reversed === cleanStr) return true;
+    else return false;
 };
 
+// Do not edit below this line
 module.exports = palindromes;
